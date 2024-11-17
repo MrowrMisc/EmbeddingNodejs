9a3f61a5148769b8a7371c52d8d7aa3d8c385bd9
Mrowr Purr <mrowr.purr@gmail.com>
2024-11-17 11:02:54 -0800

    Hello, Node.js!
    Hello, Node.js!

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# EmbeddingNodejs
-Example of embedding Node.js
+![Node.js Logo](docs/node-logo.svg)
+
+# Embedding Node.js
+
+> Example of embedding Node.js in a C++ application.
+
+At the time of writing, there are almost no examples of embedding Node.js in a C++ application. This repository aims to provide a simple example of how to do this.
+
+> Node.js : C++ embedder API
+>
+> https://nodejs.org/api/embedding.html
+
+## Instructions
+
+The following are included as git submodules:
+- `node`: https://github.com/nodejs/node
+- `v8`: https://github.com/v8/v8
+- `libuv`: https://github.com/libuv/libuv
+
+### Building Node.js
+
+Follow the instructions from `BUILDING.md` in the `node` repository
+to build Node.js for your platform:
+
+- https://github.com/nodejs/node/blob/main/BUILDING.md
+
+> Note: build Node.js in both Debug and Release mode.
+>
+> For example, on Windows simply run:
+>
+> ```bash
+> cd modules/node/
+> ./vcbuild.bat
+> ./vcbuild.bat debug
+> ```
+
+Once it's done, you should be good to go!
+
+### Build this project
+
+#### xmake
+
+```bash
+xmake     # to build
+xmake run # to run
+```
+
+#### CMake
+
+```bash
+mkdir build
+cd build
+cmake ..
+cmake --build .
+./example-of-embedding-nodejs.exe
+```
---
+++ xmake.lua
@@ -0,0 +1,29 @@
+add_rules("mode.debug", "mode.release")
+
+set_languages("c++20")
+
+target("example-of-embedding-nodejs")
+    set_kind("binary")
+    add_files("*.cpp")
+    add_includedirs(
+        "modules/node/src",
+        "modules/v8/include",
+        "modules/libuv/include"
+    )
+    add_links("ada", "brotli", "cares", "histogram", "icudata", "icui18n", "icutools", "icuucx", "libnode", "libuv", "llhttp", "nbytes", "ncrypto", "nghttp2", "nghttp3", "ngtcp2", "openssl", "simdjson", "simdutf", "sqlite", "torque_base", "uvwasi", "v8_abseil", "v8_base_without_compiler", "v8_compiler", "v8_init", "v8_initializers_slow", "v8_initializers", "v8_libbase", "v8_libplatform", "v8_snapshot", "v8_turboshaft", "v8_zlib", "zlib_adler32_simd", "zlib_inflate_chunk_simd", "zlib");
+
+    if is_mode("debug") then
+        add_defines("_ITERATOR_DEBUG_LEVEL=2")
+        add_cxflags("/MTd")
+        add_linkdirs("modules/node/out/Debug/lib")
+    else
+        add_defines("_ITERATOR_DEBUG_LEVEL=0")
+        add_cxflags("/MT")
+        add_linkdirs("modules/node/out/Release/lib")
+    end
+
+    if is_plat("windows") then
+        add_cxflags("/Zc:__cplusplus")
+        add_links("winmm", "dbghelp", "shlwapi")
+        add_links("user32", "userenv", "ole32", "shell32", "crypt32", "psapi")
+    end
